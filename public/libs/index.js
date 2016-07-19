60f141f0483765111f74f404e6788be9df1e08fb
luoshichang <1245luo@gmail.com>
2016-07-19 16:46:16 +0800

    success

---
+++ public/libs/index.js
@@ -0,0 +1,133 @@
+/**
+ * Created by shichangluo on 2016/7/19.
+ */
+$(function(){
+    var container = $('.container')
+    var box = $('.box')
+    var buddy = $('.buddy')
+    var pop = $('.pop')
+    var open = $('.btn')
+    var close = $('.close')
+    var imgs = pop.find('img')
+
+    $.Velocity.RegisterEffect('lixin.slideUpIn',{
+        defaultDuration:500,
+        calls:[
+            [{
+                opacity:[1,0],translateY:[0,100]
+            }]
+        ]
+    })
+    $.Velocity.RegisterEffect('lixin.slideDownOut',{
+        defaultDuration:300,
+        calls:[
+            [{
+                opacity:[1,0],translateY:[100,0]
+            }]
+        ]
+    })
+    $.Velocity.RegisterEffect('lixin.scaleIn',{
+        defaultDuration:300,
+        calls:[
+            [{
+                opacity:[1,0],scale:[1,0.3]
+            }]
+        ]
+    })
+    $.Velocity.RegisterEffect('lixin.scaleOut',{
+        defaultDuration:300,
+        calls:[
+            [{
+                opacity:[0,1],scale:[0.3,1]
+            }]
+        ]
+    })
+    var seqInit = [
+        {
+        elements:container,
+        properties:'lixin.slideUpIn',
+        options:{
+            delay:300
+        }
+    },
+        {
+        elements:box,
+        properties:'lixin.slideUpIn',
+        options:{
+            sequenceQueue:false
+        }
+    },
+        {
+            elements:buddy,
+            properties:'lixin.slideUpIn',
+            options:{
+                sequenceQueue:false,
+                delay:60
+            }
+        }
+    ]
+    var seqClick = [
+        {
+            elements:container,
+            properties:'lixin.slideDownOut'
+        },
+        {
+            elements:box,
+            properties:'lixin.slideDownOut',
+            options:{
+                sequenceQueue:false
+            }
+        },
+        {
+            elements:container,
+            properties:'lixin.slideUpIn'
+        },
+        {
+            elements:pop,
+            properties:'lixin.slideUpIn',
+            options:{
+                sequenceQueue:false
+            }
+        },
+        {
+            elements:imgs,
+            properties:'lixin.scaleIn'
+        }
+    ]
+    var seqClose = [
+        {
+            elements:imgs,
+            properties:'lixin.scaleOut'
+        },
+        {
+            elements:container,
+            properties:'lixin.slideDownOut'
+        },
+        {
+            elements:pop,
+            properties:'lixin.slideDownOut',
+            options:{
+                sequenceQueue:false
+            }
+        },
+        {
+            elements:container,
+            properties:'lixin.slideUpIn'
+        },
+        {
+            elements:box,
+            properties:'lixin.slideUpIn',
+            options:{
+                sequenceQueue:false
+            }
+        }
+    ]
+    $.Velocity.RunSequence(seqInit)
+    open.on('click',function(){
+        $.Velocity.RunSequence(seqClick)
+    })
+    close.on('click',function(){
+        $.Velocity.RunSequence(seqClose)
+    })
+    //
+})
